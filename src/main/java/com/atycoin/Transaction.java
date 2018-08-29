cbb0ed5d2413e08a1ef537ae960619319656e194
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-29 18:45:07 +0300

    Edit Transaction

--- src/main/java/com/atycoin/Transaction.java
+++ src/main/java/com/atycoin/Transaction.java
@@ -1,7 +1,62 @@
 package com.atycoin;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Transaction {
-    byte[] id;
-    TransactionInput txInput;
-    TransactionOutput txOutput;
+    public static final int reward = 50;
+
+    byte[] transactionId;
+    ArrayList<TransactionInput> transactionInputs;
+    ArrayList<TransactionOutput> transactionOutputs;
+
+    private Transaction(ArrayList<TransactionInput> transactionInputs, ArrayList<TransactionOutput> transactionOutputs) {
+        this.transactionInputs = transactionInputs;
+        this.transactionOutputs = transactionOutputs;
+
+        transactionId = Util.applySha256(concatenateTransactionData());
+    }
+
+    // creates a new coinbase transaction
+    public static Transaction newCoinbaseTX(String to, String data) {
+        //Reward after genesis block
+        if (data.isEmpty()) {
+            data = String.format("Reward to '%s'", to);
+        }
+
+        //Only on input, prevTransactionOutputId is empty, transactionOutputs equal to -1
+        //coinbase transaction doesn't store a script in
+        // scriptSig (Arbitrary data is stored there)
+        TransactionInput transactionInput = new TransactionInput(new byte[0], -1, data);
+
+        //reward is a reward
+        TransactionOutput transactionOutput = new TransactionOutput(reward, to);
+
+        ArrayList<TransactionInput> transactionInputs = new ArrayList<>();
+        transactionInputs.add(transactionInput);
+
+        ArrayList<TransactionOutput> transactionOutputs = new ArrayList<>();
+        transactionOutputs.add(transactionOutput);
+
+        return new Transaction(transactionInputs, transactionOutputs);
+    }
+
+    public byte[] concatenateTransactionData() {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try {
+            buffer.write(Util.changeByteOrderEndianSystem(Util.intToBytes(reward)));
+
+            for (TransactionInput transactionInput : transactionInputs) {
+                buffer.write(Util.changeByteOrderEndianSystem(transactionInput.concatenateTransactionInputData()));
+            }
+
+            for (TransactionOutput transactionOutput : transactionOutputs) {
+                buffer.write(Util.changeByteOrderEndianSystem(transactionOutput.concatenateTransactionOutputData()));
+            }
+            return buffer.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
