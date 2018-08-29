3313010d67cf9831f8af21f7966c5ff0c259bc72
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-29 04:11:32 +0300

    Add Serialize/Deserialize GSON, Utility to convert between little-endian and big-endian, Util to convert between byte array to hex (to present hashing) and Util to convert bytes to string

--- src/main/java/com/atycoin/Util.java
+++ src/main/java/com/atycoin/Util.java
@@ -1,142 +1,171 @@
 package com.atycoin;
 
+//TODO: BASE58 Encoding
+//TODO: Convert key to Address
+//TODO: Checksum
+//TODO: Clean Utility class
+
+import com.google.gson.Gson;
+import org.bouncycastle.util.Arrays;
+
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
-import java.security.spec.ECGenParameterSpec;
+import java.util.Base64;
 
 public class Util {
     public static void main(String[] args) {
         String msg = "Ahmad";
-        byte[] msgHashed = hash(msg);
-        System.out.print("Message hash: ");
-        System.out.println(toHexString(msgHashed));
+        byte[] msgHashed = applySha256(msg.getBytes(StandardCharsets.UTF_8));
+        System.out.print("Message hashing: ");
+        System.out.println(bytesToHex(msgHashed));
         System.out.println();
-
-        KeyPair keyPair = generateKeyPair();
-        PrivateKey privateKey = keyPair.getPrivate();
-        PublicKey publicKey = keyPair.getPublic();
-
-        System.out.print("Private key: ");
-        System.out.println(toHexString(privateKey.getEncoded()));
-        System.out.print("Encoded Private Key: ");
-        System.out.println();
-
-        System.out.print("Public key: ");
-        System.out.println(toHexString(publicKey.getEncoded()));
-        System.out.print("Encoded Public key: ");
-        System.out.println();
-
-        byte[] signature = createSignature(privateKey, msgHashed);
-        System.out.print("Message Signature: ");
-        System.out.println(toHexString(signature));
-
-        boolean bool = verifySignature(publicKey, msgHashed, signature);
-
-        if (bool) {
-            System.out.println("Signature verified");
-        } else {
-            System.out.println("Signature failed");
-        }
+//        KeyPair keyPair = generateKeyPair();
+//        PrivateKey privateKey = keyPair.getPrivate();
+//        PublicKey publicKey = keyPair.getPublic();
+//
+//        System.out.print("Private key: ");
+//        System.out.println(toHexString(privateKey.getEncoded()));
+//        System.out.print("Encoded Private Key: ");
+//        System.out.println();
+//
+//        System.out.print("Public key: ");
+//        System.out.println(toHexString(publicKey.getEncoded()));
+//        System.out.print("Encoded Public key: ");
+//        System.out.println();
+//
+//        byte[] signature = createSignature(privateKey, msgHashed);
+//        System.out.print("Message Signature: ");
+//        System.out.println(toHexString(signature));
+//
+//        boolean bool = verifySignature(publicKey, msgHashed, signature);
+//
+//        if (bool) {
+//            System.out.println("Signature verified");
+//        } else {
+//            System.out.println("Signature failed");
+//        }
     }
 
-    public static byte[] hash(String message) {
-        byte[] digest = {0};
+    public static byte[] applySha256(byte[] input) {
         try {
-            //Creating the MessageDigest object
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest digest = MessageDigest.getInstance("SHA-256", "BC");
 
-            //Passing data to the created MessageDigest Object
-            md.update(message.getBytes(StandardCharsets.UTF_8));
-
-            //Compute the message digest
-            digest = md.digest();
-
-            return digest;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            //Applies sha256 to our input
+            return digest.digest(input);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
         }
-
-        return digest;
     }
 
-    public static String toHexString(byte[] hashValue) {
-        //Converting the byte array in to HexString format
-        StringBuffer hexString = new StringBuffer();
-
-        for (int i = 0; i < hashValue.length; i++) {
-            String hex = Integer.toHexString(0xff & hashValue[i]);
-            if (hex.length() == 1)
-                hexString.append('0');
-            hexString.append(hex); // 0xFF & ff ff ff fe = fe
+    // Applies ECDSA Signature and returns the result (as bytes)
+    public static byte[] applyECDSASig(PrivateKey privateKey, byte[] input) {
+        try {
+            Signature dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            dsa.update(input);
+            return dsa.sign();
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
         }
+    }
 
+    //Verifies a String signature
+    public static boolean verifyECDSASig(PublicKey publicKey, byte[] data, byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data);
+            return ecdsaVerify.verify(signature);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeyException | SignatureException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    //Returns difficulty string target, to compare to hash
+    public static String getDifficultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    //TODO: fix bugs in merkleRoot Method
+//    //TODO: ODD issue
+//    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+//        int count = transactions.size();
+//
+//        ArrayList<String> previousTreeLayer = new ArrayList<>();
+//        for (Transaction transaction : transactions) {
+//            previousTreeLayer.add(transaction.transactionId);
+//        }
+//
+//        ArrayList<String> treeLayer = previousTreeLayer;
+//
+//        while (count > 1) {
+//            treeLayer = new ArrayList<>();
+//            for (int i = 1; i < previousTreeLayer.size(); i += 2) {
+//                treeLayer.add(applySha256(previousTreeLayer.get(i - 1) + previousTreeLayer.get(i)));
+//            }
+//
+//            count = treeLayer.size();
+//            previousTreeLayer = treeLayer;
+//        }
+//
+//        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+//        return merkleRoot;
+//    }
+
+    public static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(); // This will contain hash as hexidecimal
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
         return hexString.toString();
     }
 
-    public static KeyPair generateKeyPair() {
-
-        KeyPair keyPair = null;
-        try {
-            //Creating KeyPair generator object
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-
-
-            //Initializing the KeyPairGenerator
-            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256k1");
-            SecureRandom secureRandom = new SecureRandom();
-            keyPairGenerator.initialize(ecGenParameterSpec, secureRandom);
-
-            keyPair = keyPairGenerator.generateKeyPair();
-            return keyPair;
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        }
-
-        return keyPair;
+    public static byte[] longToBytes(long input) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Long.BYTES);
+        byteBuffer.putLong(input);
+        return byteBuffer.array();
     }
 
-    public static byte[] createSignature(PrivateKey privateKey, byte[] data) {
-        byte[] digitalSignature = {0};
-        try {
-            //Creating a Signature object
-            Signature signature = Signature.getInstance("SHA256withECDSA");
-
-
-            //Initialize the signature
-            SecureRandom secureRandom = new SecureRandom();
-            signature.initSign(privateKey, secureRandom);
-
-            //Adding data to the signature
-            signature.update(data);
-
-            //Calculating the signature
-            digitalSignature = signature.sign();
-            return digitalSignature;
-
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
-            e.printStackTrace();
-        }
-
-        return digitalSignature;
+    public static byte[] intToBytes(int input) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.BYTES);
+        byteBuffer.putInt(input);
+        return byteBuffer.array();
     }
 
-    public static boolean verifySignature(PublicKey publicKey, byte[] data, byte[] signatureToVerified) {
-        try {
-            //Creating a Signature object
-            Signature signature = Signature.getInstance("SHA256withECDSA");
+    public static byte[] changeByteOrderEndianSystem(byte[] input) {
+        return Arrays.reverse(input);
+    }
 
-            //Initializing the signature
-            signature.initVerify(publicKey);
+    public static Block deserializeBlock(String serializedBlock) {
+        Gson gson = new Gson();
+        return gson.fromJson(serializedBlock, Block.class);
+    }
 
-            //Update the data to be verified
-            signature.update(data);
+//    public static String bigIntegerToHex(BigInteger bigInteger) {
+//        return bigInteger.toString(16);
+//    }
 
-            //Verifying the signature
-            return signature.verify(signatureToVerified);
-        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
-            e.printStackTrace();
-        }
+//    public static byte[] stringToBytes(String input) {
+//        return input.getBytes(StandardCharsets.UTF_8);
+//    }
 
-        return false;
+    // Short hand helper to turn Object into a json string
+    public static String serializeHash(byte[] hash) {
+        return new Gson().toJson(hash);
+    }
+
+    public static byte[] deserializeHash(String serializedHash) {
+        return new Gson().fromJson(serializedHash, byte[].class);
+    }
+
+    public static String bytesToString(byte[] input) {
+        return new String(input);
     }
 }
