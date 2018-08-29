c543570ad68d24d6927b71d09297f27b0f4d7e69
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-29 18:44:34 +0300

    Edit TransactionInput and TransactionOutput

--- src/main/java/com/atycoin/TransactionInput.java
+++ src/main/java/com/atycoin/TransactionInput.java
@@ -1,4 +1,29 @@
 package com.atycoin;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class TransactionInput {
+    byte[] prevTransactionOutputId;
+    int coinAmount;
+    String scriptSig;
+
+    public TransactionInput(byte[] prevTransactionOutputId, int coinAmount, String scriptSig) {
+        this.prevTransactionOutputId = prevTransactionOutputId;
+        this.coinAmount = coinAmount;
+        this.scriptSig = scriptSig;
+    }
+
+    public byte[] concatenateTransactionInputData() {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try {
+            buffer.write(Util.changeByteOrderEndianSystem(prevTransactionOutputId));
+            buffer.write(Util.changeByteOrderEndianSystem(Util.intToBytes(coinAmount)));
+            buffer.write(Util.changeByteOrderEndianSystem(Util.stringToBytes(scriptSig)));
+
+            return buffer.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/atycoin/TransactionOutput.java
+++ src/main/java/com/atycoin/TransactionOutput.java
@@ -1,4 +1,26 @@
 package com.atycoin;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class TransactionOutput {
+    int coinAmount;
+    String scriptPubKey;
+
+    public TransactionOutput(int coinAmount, String scriptPubKey) {
+        this.coinAmount = coinAmount;
+        this.scriptPubKey = scriptPubKey;
+    }
+
+    public byte[] concatenateTransactionOutputData() {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try {
+            buffer.write(Util.changeByteOrderEndianSystem(Util.intToBytes(coinAmount)));
+            buffer.write(Util.changeByteOrderEndianSystem(Util.stringToBytes(scriptPubKey)));
+
+            return buffer.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
