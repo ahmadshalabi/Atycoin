6ddda1ece54a5529514f6953e74f647a7fe99c62
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-29 18:43:14 +0300

    Remove unused and commented methods, Also add string to bytes

--- src/main/java/com/atycoin/Util.java
+++ src/main/java/com/atycoin/Util.java
@@ -14,39 +14,6 @@ import java.security.*;
 import java.util.Base64;
 
 public class Util {
-    public static void main(String[] args) {
-        String msg = "Ahmad";
-        byte[] msgHashed = applySha256(msg.getBytes(StandardCharsets.UTF_8));
-        System.out.print("Message hashing: ");
-        System.out.println(bytesToHex(msgHashed));
-        System.out.println();
-//        KeyPair keyPair = generateKeyPair();
-//        PrivateKey privateKey = keyPair.getPrivate();
-//        PublicKey publicKey = keyPair.getPublic();
-//
-//        System.out.print("Private key: ");
-//        System.out.println(toHexString(privateKey.getEncoded()));
-//        System.out.print("Encoded Private Key: ");
-//        System.out.println();
-//
-//        System.out.print("Public key: ");
-//        System.out.println(toHexString(publicKey.getEncoded()));
-//        System.out.print("Encoded Public key: ");
-//        System.out.println();
-//
-//        byte[] signature = createSignature(privateKey, msgHashed);
-//        System.out.print("Message Signature: ");
-//        System.out.println(toHexString(signature));
-//
-//        boolean bool = verifySignature(publicKey, msgHashed, signature);
-//
-//        if (bool) {
-//            System.out.println("Signature verified");
-//        } else {
-//            System.out.println("Signature failed");
-//        }
-    }
-
     public static byte[] applySha256(byte[] input) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256", "BC");
@@ -82,40 +49,9 @@ public class Util {
         }
     }
 
-    //Returns difficulty string target, to compare to hash
-    public static String getDifficultyString(int difficulty) {
-        return new String(new char[difficulty]).replace('\0', '0');
-    }
-
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
-
-    //TODO: fix bugs in merkleRoot Method
-//    //TODO: ODD issue
-//    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
-//        int count = transactions.size();
-//
-//        ArrayList<String> previousTreeLayer = new ArrayList<>();
-//        for (Transaction transaction : transactions) {
-//            previousTreeLayer.add(transaction.transactionId);
-//        }
-//
-//        ArrayList<String> treeLayer = previousTreeLayer;
-//
-//        while (count > 1) {
-//            treeLayer = new ArrayList<>();
-//            for (int i = 1; i < previousTreeLayer.size(); i += 2) {
-//                treeLayer.add(applySha256(previousTreeLayer.get(i - 1) + previousTreeLayer.get(i)));
-//            }
-//
-//            count = treeLayer.size();
-//            previousTreeLayer = treeLayer;
-//        }
-//
-//        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
-//        return merkleRoot;
-//    }
 
     public static String bytesToHex(byte[] hash) {
         StringBuilder hexString = new StringBuilder(); // This will contain hash as hexidecimal
@@ -125,6 +61,10 @@ public class Util {
             hexString.append(hex);
         }
         return hexString.toString();
+    }
+
+    public static byte[] stringToBytes(String input) {
+        return input.getBytes(StandardCharsets.UTF_8);
     }
 
     public static byte[] longToBytes(long input) {
@@ -147,14 +87,6 @@ public class Util {
         Gson gson = new Gson();
         return gson.fromJson(serializedBlock, Block.class);
     }
-
-//    public static String bigIntegerToHex(BigInteger bigInteger) {
-//        return bigInteger.toString(16);
-//    }
-
-//    public static byte[] stringToBytes(String input) {
-//        return input.getBytes(StandardCharsets.UTF_8);
-//    }
 
     // Short hand helper to turn Object into a json string
     public static String serializeHash(byte[] hash) {
