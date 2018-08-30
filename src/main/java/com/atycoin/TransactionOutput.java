15d5b931b1b6a61376e2b35a989a5a783eb8feca
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-30 12:24:40 +0300

    add canBeUnlockedWith, and fix concatenation logic

--- src/main/java/com/atycoin/TransactionOutput.java
+++ src/main/java/com/atycoin/TransactionOutput.java
@@ -4,23 +4,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class TransactionOutput {
-    int coinAmount;
-    String scriptPubKey;
+    public int value;
+    public String scriptPubKey;
 
-    public TransactionOutput(int coinAmount, String scriptPubKey) {
-        this.coinAmount = coinAmount;
+    public TransactionOutput(int value, String scriptPubKey) {
+        this.value = value;
         this.scriptPubKey = scriptPubKey;
     }
 
     public byte[] concatenateTransactionOutputData() {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         try {
-            buffer.write(Util.changeByteOrderEndianSystem(Util.intToBytes(coinAmount)));
+            //little-endian
+            buffer.write(Util.changeByteOrderEndianSystem(Util.intToBytes(value)));
             buffer.write(Util.changeByteOrderEndianSystem(Util.stringToBytes(scriptPubKey)));
 
             return buffer.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //checks if the output can be unlocked with the provided data
+    public boolean canBeUnlockedWith(String unlockingData) {
+        return unlockingData.equals(scriptPubKey);
     }
 }
