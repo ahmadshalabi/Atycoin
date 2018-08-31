55b0d3b07522fd5b5e562fc1e973ab1cd16cb479
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-08-31 15:44:04 +0300

    Add BASE58 encode(To get Address in Base58)

---
+++ src/main/java/com/atycoin/Base58.java
@@ -0,0 +1,42 @@
+package com.atycoin;
+
+import java.math.BigInteger;
+
+public class Base58 {
+    private static final char[] ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".toCharArray();
+    private static final String ALPHABET_String = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    private static final BigInteger BASE58 = new BigInteger(String.valueOf(ALPHABET.length));
+    private static final int POSITIVE_SIGNUM = 1;
+    String str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+    // encodes a byte array to Base58
+    public static byte[] encode(byte[] input) {
+        if (input.length == 0) {
+            return new byte[0];
+        }
+
+        StringBuilder buffer = new StringBuilder();
+
+        // SumOf(in * 2^(i*8)
+        BigInteger sumOfInputInBase8 = new BigInteger(POSITIVE_SIGNUM, input);
+
+        BigInteger[] quotientAndRemainder;
+        while (sumOfInputInBase8.compareTo(BigInteger.ZERO) != 0) {
+            quotientAndRemainder = sumOfInputInBase8.divideAndRemainder(BASE58);
+            sumOfInputInBase8 = quotientAndRemainder[0]; // quotient
+            //Append at beginning of buffer
+            buffer.insert(0, ALPHABET[quotientAndRemainder[1].intValue()]); // Map Remainder to Alphabet
+        }
+
+        // Convert leading zeros
+        for (byte element : input) {
+            if (element == 0x00) {
+                buffer.insert(0, ALPHABET[0]);
+            } else {
+                break;
+            }
+        }
+
+        return buffer.toString().getBytes();
+    }
+}
