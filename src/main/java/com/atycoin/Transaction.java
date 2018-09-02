8d6d7657e6070540d5c5071933b6c8373b47746b
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-09-02 05:26:29 +0300

    Add sign, verify, trimmedCopy and Override toString

--- src/main/java/com/atycoin/Transaction.java
+++ src/main/java/com/atycoin/Transaction.java
@@ -1,5 +1,7 @@
 package com.atycoin;
 
+import org.bouncycastle.jce.interfaces.ECPrivateKey;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,17 +19,6 @@ public class Transaction {
         this.inputs = inputs;
         this.outputs = outputs;
     }
-
-//    public void sign(ECPrivateKey privateKey, HashMap<String, Transaction> previousTransactions) {
-//        if (isCoinbaseTransaction()) {
-//            return;
-//        }
-//
-//        for (TransactionInput input : inputs) {
-//            String id = Util.
-//            if (previousTransactions.get())
-//        }
-//    }
 
     // creates a new coinbase transaction (to : address in BASE58)
     public static Transaction newCoinbaseTransaction(String to) {
@@ -49,7 +40,7 @@ public class Transaction {
         transactionOutputs.add(transactionOutput);
 
         Transaction coinbaseTransaction = new Transaction(transactionInputs, transactionOutputs);
-        coinbaseTransaction.hash();
+        coinbaseTransaction.id = coinbaseTransaction.hash();
         return coinbaseTransaction;
     }
 
@@ -60,14 +51,15 @@ public class Transaction {
 
         Blockchain blockchain = Blockchain.getInstance();
 
-        WalletsProcessor walletsProcessor = WalletsProcessor.newWalletsProcessor();
+        Wallets wallets = Wallets.newWallets();
 
         //Get wallet rather than decode from address
         // Because I need rawPublicKey that sored in wallet
         // when reference transactionInput as spent
-        Wallet wallet = walletsProcessor.getWallet(from);
+        Wallet wallet = wallets.getWallet(from);
         byte[] fromPublicKeyHashed = Wallet.hashPublicKey(wallet.getRawPublicKey());
 
+        // finds and returns unspent outputs to reference in inputs
         ArrayList<Transaction> unspentTransactions = blockchain.findUnspentTransaction(fromPublicKeyHashed);
         int accumulated = 0;
 
@@ -127,12 +119,96 @@ public class Transaction {
         }
 
         Transaction newTransaction = new Transaction(inputs, outputs);
-        newTransaction.hash();
+        newTransaction.id = newTransaction.hash();
+        blockchain.signTransaction(newTransaction, wallet.getPrivateKey());
 
         return newTransaction;
     }
 
-    public void hash() {
+    //    signs each input of a Transaction
+    public void sign(ECPrivateKey privateKey, HashMap<String, Transaction> prevTXs) {
+        if (isCoinbaseTransaction()) {
+            return;
+        }
+
+        for (TransactionInput input : inputs) {
+            if (prevTXs.get(Util.serializeHash(input.prevTransactionId)).id == null) {
+                System.out.println("ERROR: Previous transaction is not correct");
+                return;
+            }
+        }
+
+        Transaction txCopy = trimmedCopy();
+
+        for (TransactionInput input : txCopy.inputs) {
+            Transaction prevTx = prevTXs.get(Util.serializeHash(input.prevTransactionId));
+
+            input.signature = new byte[0];
+            input.rawPublicKey = prevTx.outputs.get(input.transactionOutputIndex).publicKeyHashed;
+
+            txCopy.id = txCopy.hash();
+            input.rawPublicKey = new byte[0];
+
+            int txCopyIndex = txCopy.inputs.indexOf(input);
+            inputs.get(txCopyIndex).signature = Util.applyECDSASig(privateKey, txCopy.id);
+        }
+    }
+
+    // verifies signatures of Transaction inputs
+    public boolean verify(Map<String, Transaction> prevTXs) {
+        if (isCoinbaseTransaction()) {
+            return true;
+        }
+
+        for (TransactionInput input : inputs) {
+            if (prevTXs.get(Util.serializeHash(input.prevTransactionId)).id == null) {
+                System.out.println("ERROR: Previous transaction is not correct");
+                return false;
+            }
+        }
+        Transaction txCopy = trimmedCopy();
+
+        boolean isValidTransaction = true;
+        for (TransactionInput input : inputs) {
+            Transaction prevTX = prevTXs.get(Util.serializeHash(input.prevTransactionId));
+
+            int inputIndex = inputs.indexOf(input);
+            txCopy.inputs.get(inputIndex).signature = new byte[0];
+            txCopy.inputs.get(inputIndex).rawPublicKey =
+                    prevTX.outputs.get(input.transactionOutputIndex).publicKeyHashed;
+
+            txCopy.id = txCopy.hash();
+            txCopy.inputs.get(inputIndex).rawPublicKey = new byte[0];
+
+            isValidTransaction = Util.verifyECDSASig(Util.decodeKey(inputs.get(inputIndex).rawPublicKey),
+                    txCopy.id, inputs.get(inputIndex).signature);
+
+            if (!isValidTransaction) {
+                break;
+            }
+        }
+
+        return isValidTransaction;
+    }
+
+    // creates a trimmed copy of Transaction to be used in signing
+    public Transaction trimmedCopy() {
+        ArrayList<TransactionInput> inputs = new ArrayList<>();
+        ArrayList<TransactionOutput> outputs = new ArrayList<>();
+
+        for (TransactionInput input : this.inputs) {
+            inputs.add(new TransactionInput(
+                    input.prevTransactionId, input.transactionOutputIndex, new byte[0], new byte[0]));
+        }
+
+        for (TransactionOutput output : this.outputs) {
+            outputs.add(new TransactionOutput(output.value, output.publicKeyHashed));
+        }
+
+        return new Transaction(inputs, outputs);
+    }
+
+    public byte[] hash() {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         try {
             //little-endian
@@ -140,18 +216,16 @@ public class Transaction {
 
             //Already little-endian from input itself
             for (TransactionInput transactionInput : inputs) {
-                buffer.write(transactionInput.concatenateTransactionInputData());
+                buffer.write(transactionInput.concatenateData());
             }
 
             //Already little-endian from output itself
             for (TransactionOutput transactionOutput : outputs) {
-                buffer.write(transactionOutput.concatenateTransactionOutputData());
+                buffer.write(transactionOutput.concatenateData());
             }
 
-            id = Util.applySHA256(buffer.toByteArray());
-
             // Big-endian
-            id = Util.reverseBytesOrder(id);
+            return Util.reverseBytesOrder(Util.applySHA256(buffer.toByteArray()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -165,5 +239,31 @@ public class Transaction {
         return inputs.size() == 1 &&
                 inputs.get(0).prevTransactionId.length == 0 &&
                 inputs.get(0).transactionOutputIndex == -1;
+    }
+
+    // returns a human-readable representation of a transaction
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("--- Transaction %s:", Util.bytesToHex(id)));
+
+        TransactionInput input;
+        for (int i = 0, size = inputs.size(); i < size; i++) {
+            input = inputs.get(i);
+            builder.append(String.format("     Input %d%n:", i));
+            builder.append(String.format("       TXID:      %s%n", Util.bytesToHex(input.prevTransactionId)));
+            builder.append(String.format("       OutIndex:  %d%n", input.transactionOutputIndex));
+            builder.append(String.format("       Signature: %s%n", Util.bytesToHex(input.signature)));
+            builder.append(String.format("       PubKey:    %s%n:", Util.bytesToHex(input.rawPublicKey)));
+        }
+
+        TransactionOutput output;
+        for (int i = 0, size = outputs.size(); i < size; i++) {
+            output = outputs.get(i);
+            builder.append(String.format("     Output %d%n:", i));
+            builder.append(String.format("       Value:  %d%n", output.value));
+            builder.append(String.format("       Script: %s%n", Util.bytesToHex(output.publicKeyHashed)));
+        }
+        return builder.toString();
     }
 }
