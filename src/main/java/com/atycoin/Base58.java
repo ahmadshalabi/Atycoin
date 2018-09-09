b4672d65a19003ef21934ffb89c303ffe7c8280d
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-09-09 17:29:39 +0300

    Refactor Base58

--- src/main/java/com/atycoin/Base58.java
+++ src/main/java/com/atycoin/Base58.java
@@ -4,30 +4,33 @@ import java.math.BigInteger;
 
 public class Base58 {
     private static final String ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    private static final String EMPTY_ADDRESS = "";
     private static final BigInteger BASE58 = new BigInteger(String.valueOf(58));
     private static final int POSITIVE_SIGN = 1;
 
     // encodes a byte array to Base58
     public static String encode(byte[] fullPayload) {
         if (fullPayload.length == 0) {
-            return "";
+            return EMPTY_ADDRESS;
         }
-
-        StringBuilder address = new StringBuilder();
 
         // SumOf(in * 2^(i*8))
         BigInteger sumOfDigitBase58 = new BigInteger(POSITIVE_SIGN, fullPayload);
 
+        StringBuilder address = new StringBuilder();
         BigInteger[] quotientAndRemainder;
         while (sumOfDigitBase58.compareTo(BigInteger.ZERO) != 0) {
             quotientAndRemainder = sumOfDigitBase58.divideAndRemainder(BASE58);
             sumOfDigitBase58 = quotientAndRemainder[0]; // quotient
-            //Append at beginning of address
-            address.insert(0, ALPHABET.charAt(quotientAndRemainder[1].intValue())); // Map Remainder to Alphabet
+            BigInteger remainder = quotientAndRemainder[1];
+            int alphabetIndex = remainder.intValue();
+
+            //Insert at beginning of address
+            address.insert(0, ALPHABET.charAt(alphabetIndex)); // Map Remainder to Alphabet
         }
 
         //Version byte [Leading Zero]
-        if (fullPayload[0] == 0x00) {
+        if (fullPayload[0] == 0) {
             address.insert(0, ALPHABET.charAt(0));
         }
 
@@ -37,23 +40,28 @@ public class Base58 {
     // decodes Base58-encoded data
     public static byte[] decode(String address) {
         if (address.isEmpty()) {
-            return new byte[0];
+            return Constant.EMPTY_BYTE_ARRAY;
         }
 
-        BigInteger sumOfDigitBase58 = BigInteger.ZERO;
+        BigInteger sumOfDigits = BigInteger.ZERO;
 
-        // sumOfDigitBase58 * 58 + indexOF(digit)
-        for (char digit : address.toCharArray()) {
-            sumOfDigitBase58 = sumOfDigitBase58.multiply(BASE58);
-            sumOfDigitBase58 = sumOfDigitBase58.add(BigInteger.valueOf(ALPHABET.indexOf(digit)));
+        // sumOfDigits * 58 + indexOf(digit)
+        char[] addressDigits = address.toCharArray();
+        for (char digit : addressDigits) {
+            sumOfDigits = sumOfDigits.multiply(BASE58);
+
+            int index = ALPHABET.indexOf(digit);
+            BigInteger valueOfIndex = BigInteger.valueOf(index);
+            sumOfDigits = sumOfDigits.add(valueOfIndex);
         }
 
         // if fullPayloadWithMissingZeros length less than 25, there are leading zero
         byte[] fullPayload = new byte[25];
-        byte[] fullPayloadWithMissingZeros = sumOfDigitBase58.toByteArray();
-        int numOfLeadingZero = fullPayload.length - fullPayloadWithMissingZeros.length;
+        byte[] fullPayloadWithMissingZeros = sumOfDigits.toByteArray();
+        int numberOfLeadingZero = fullPayload.length - fullPayloadWithMissingZeros.length;
+
         System.arraycopy(fullPayloadWithMissingZeros, 0,
-                fullPayload, numOfLeadingZero, fullPayloadWithMissingZeros.length);
+                fullPayload, numberOfLeadingZero, fullPayloadWithMissingZeros.length);
 
         return fullPayload;
     }
