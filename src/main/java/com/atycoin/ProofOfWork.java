79db9e45243f4064a5bbbb45844eaafbf9a1c4bb
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2018-09-10 01:52:02 +0300

    Refactor ProofOfWork

--- src/main/java/com/atycoin/ProofOfWork.java
+++ src/main/java/com/atycoin/ProofOfWork.java
@@ -2,32 +2,35 @@ package com.atycoin;
 
 public class ProofOfWork {
     private final Block block;
-    private final int targetBits;
+    private final int targetBytes;
+    private final int lastByteTarget;
     private byte[] blockHeader;
 
     public ProofOfWork(Block block) {
         this.block = block;
-        targetBits = block.getTargetBits();
+
+        //calculate target
+        int targetBits = block.getTargetBits();
+        targetBytes = targetBits / 8;
+        int lastTargetBits = targetBits % 8;
+        lastByteTarget = ((1 << 8 - lastTargetBits) - 1) & 0xff;
     }
 
+    //TODO: Add extraNonce when nonce overflow occurs
     public int runProofOfWork() {
         //initialize blockHeader
         int nonce = 0;
         blockHeader = block.setBlockHeader(nonce);
-
-        //TODO: Add extraNonce when nonce overflow occurs
         while (nonce < Integer.MAX_VALUE) {
-            updateBlockHeader(nonce++);
+            updateBlockHeader(nonce);
 
             // Double hash
             byte[] hash = Util.applySHA256(Util.applySHA256(blockHeader));
 
-            // Change to Big-endian
-            hash = Util.reverseBytesOrder(hash);
-
             if (isHashMeetTarget(hash)) {
                 break;
             }
+            nonce++;
         }
         return nonce;
     }
@@ -39,23 +42,15 @@ public class ProofOfWork {
     }
 
     private boolean isHashMeetTarget(byte[] hash) {
-        // TODO: After make target Adjustable move numberOfBytes and bitsInLastByte Outside this function
-        int numberOfBytes = targetBits / 8;
-        int bitsInLastByte = targetBits % 8;
-
-        // Check whole bytes
-        for (int byteIndex = 0; byteIndex < numberOfBytes; byteIndex++) {
-            if (hash[byteIndex] != 0) {
+        int index;
+        int targetIndex = hash.length - targetBytes;
+        for (index = hash.length - 1; index >= targetIndex; index--) {
+            if (hash[index] != 0) {
                 return false;
             }
         }
 
-        int unsignedLastHashByte = hash[numberOfBytes] & 0xff; // convert to unsigned byte
-
-        // ex: bitsInLastByte = 5 --->  (1 << 8 - 5) = 0000 1000 --> 00001000 = 00000111
-        int unsignedTargetInLastByte = ((1 << 8 - bitsInLastByte) - 1) & 0xff; // Bitmask
-
-        // unsignedTargetInLastByte : Upper Boundary
-        return unsignedLastHashByte <= unsignedTargetInLastByte;
+        int lastByteInHash = hash[index];
+        return lastByteInHash <= lastByteTarget;
     }
 }
